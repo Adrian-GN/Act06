1e38eee5f19aff72b53b6d91880ac8613afc1ec2
Adrian Garcia <adrian.garcia2250@alumnos.udg.mx>
2020-09-28 20:33:40 -0500

    Verificado

---
+++ Act6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main(){
+	int f;
+	printf("Esto funciona?? 1=si 2=no\n");
+	scanf("%i", &f);
+	if(f==1)
+	printf("\nWujuu genial!!");
+	else
+	printf("\nQue?? diablos");
+	return 0;
+}
